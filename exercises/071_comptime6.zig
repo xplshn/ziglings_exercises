63c798637cbb8b73ac0348933cc8dc42bfa9810a
Luka Markušić <markusicluka@gmail.com>
2026-06-01 15:37:05 +0200

    Fix 071_comptime6.zig because of new build system

--- exercises/071_comptime6.zig
+++ exercises/071_comptime6.zig
@@ -36,13 +36,14 @@ pub fn main() void {
     // statement was repeated three times almost verbatim. Yuck!
     //
     // Please use an 'inline for' to implement the block below
-    // for each field in the slice 'fields'!
+    // for each field in the corresponding slices (they're of the same length)!
 
-    const fields = @typeInfo(Narcissus).@"struct".fields;
+    const field_names = @typeInfo(Narcissus).@"struct".field_names;
+    const field_types = @typeInfo(Narcissus).@"struct".field_types;
 
     ??? {
-        if (field.type != void) {
-            print(" {s}", .{field.name});
+        if (field_type != void) {
+            print(" {s}", .{field_name});
         }
     }
 
